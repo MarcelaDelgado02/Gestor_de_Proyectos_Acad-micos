47967079114eb7c1772924e6645657cc5dff11b2
Vale aguero <valeryaguero27@gmail.com>
2025-11-12 20:51:34 -0600

    Se crean constructores de reportes

--- Entidades/Reporte.cs
+++ Entidades/Reporte.cs
@@ -2,5 +2,34 @@
 {
     public class Reporte
     {
+        public string NombreProyecto { get; set; }
+        public int TotalTareas { get; set; }
+        public int TareasCompletadas { get; set; }
+        public decimal PorcentajeAvance {  get; set; }  
+           
     }
+
+    public class ReporteEstudianteProyecto
+    {
+        public string NombreEstudiante { get; set; }
+        public int TareasAsignadas { get; set; }
+        public int TareasCompletadas { get; set; }
+        public decimal PorcentajeCompletado { get; set; }
+
+
+    }
+
+    public class ReportePersonal
+    {
+        public string NombrePersonal { get; set; }
+        public string NombreTarea  { get; set; }
+        public string Estado {  get; set; } 
+        public DateTime? FechaCompletado { get; set; }  
+        public DateTime FechaVencimiento { get; set; }
+
+
+
 }
+
+    
+    
