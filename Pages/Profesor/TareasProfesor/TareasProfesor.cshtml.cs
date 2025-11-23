c870b585acec9df15b994bff512bcfec0e7dfc43
Marcela Abarca Delgado <admarcela5@gmail.com>
2025-11-23 10:38:34 -0600

    Cambios graficos
    Se agregan detalles de iconos

---
+++ Pages/Profesor/TareasProfesor/TareasProfesor.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gestor_de_Proyectos_Académicos.Pages.Profesor.TareasProfesor
+{
+    public class TareasProfesorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
