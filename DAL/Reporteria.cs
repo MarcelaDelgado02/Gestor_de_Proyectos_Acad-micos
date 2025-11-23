97fb9d7b7caf9b9e46fa6ab9487be4019a6f6066
Vale aguero <valeryaguero27@gmail.com>
2025-11-23 14:00:17 -0600

    ReporteriaDAL
    se crea la capa de Acceso a Datos

--- DAL/Reporteria.cs
+++ DAL/Reporteria.cs
@@ -1,7 +1,57 @@
 ﻿using Gestor_de_Proyectos_Académicos.Entidades;
+using Microsoft.Data.SqlClient;
 using Microsoft.Identity.Client;
+using System.Data;
 
 namespace Gestor_de_Proyectos_Académicos.DAL
 {
 
+
+    public class ReporteriaDAL
+    {
+
+        private readonly string _connectionString;
+        private object proyectoId;
+
+        public ReporteriaDAL(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<Reporte> ObtenerReporteProyectos(RepoteFiltros filtros)
+
+        {
+            var reporte = new List<Reporte>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                using (var command = new SqlCommand("SP_ReporteProyectos",connection))
+
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@ProyectosId", proyectoId);
+
+
+                    connection.Open();
+                    using(var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            reporte.Add(new Reporte
+                            {
+                                EstudianteId = reader.GetInt32("EstudianteId"),
+                                NombreEstudiate = reader.GetString("NombreEstudiante"),
+                                TareasCompletadasEstudiante = reader.GetInt32("TareasCompletadas"),
+                                PorcentajeAvance = reader.GetDecimal("PorcentajeAvancePersonal")
+                            });
+
+
+                        }
+
+                    }
+                }
+            }
+            return reporte;
+        }
+    }
 }
