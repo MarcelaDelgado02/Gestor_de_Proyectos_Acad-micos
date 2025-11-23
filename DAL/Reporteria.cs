bbabd106bd69d548b4f7e55a825358a0b805ccbc
Vale aguero <valeryaguero27@gmail.com>
2025-11-23 13:16:56 -0600

    Reporte (Capa de Entidades)
    se crea los atributos y los constructores que se van a usar en la capa DAL

--- DAL/Reporteria.cs
+++ DAL/Reporteria.cs
@@ -3,29 +3,5 @@ using Microsoft.Identity.Client;
 
 namespace Gestor_de_Proyectos_Académicos.DAL
 {
-    public class Reporteria
-    {
 
-        private readonly ConexionBD_conexion;
-
-            public List<Reporte> ObtenerReporteProyecto(int? profesorid = null)
-        { 
-         
-            var query = @"SELECT p.Nombre as NombreProyecto,
-                             COUNT(t.Id) as TotalTareas,
-                             SUM(CASE WHEN t.Estado = 'Completado' THEN 1 ELSE 0 END) as TareasCompletadas
-                      FROM Proyectos p
-                      LEFT JOIN Tareas t ON p.Id = t.ProyectoId
-                      WHERE (@profesorId IS NULL OR p.ProfesorId = @profesorId)
-                      GROUP BY p.Id, p.Nombre";
-
-        }
-
-        public List<ReporteEstudianteProyecto>  ObtenerReporteEstudianteProyectos(int proyectoId)
-
-        {
-          public List<ReportePersonal> ObtenerReortePersonal(int estudianteId)
-
-    }
-    }
 }
--- Entidades/Reporte.cs
+++ Entidades/Reporte.cs
@@ -1,35 +1,39 @@
-﻿namespace Gestor_de_Proyectos_Académicos.Entidades
+﻿using System.ComponentModel;
+using System.Data;
+
+namespace Gestor_de_Proyectos_Académicos.Entidades
 {
     public class Reporte
     {
+
+        public int ProyectoId { get; set; }
         public string NombreProyecto { get; set; }
         public int TotalTareas { get; set; }
-        public int TareasCompletadas { get; set; }
-        public decimal PorcentajeAvance {  get; set; }  
-           
+        public int TareasCompletas { get; set; }
+        public decimal PorcentajeAvance { get; set; }
+        public int EstudianteId { get; set; }
+        public string NombreEstudiate { get; set; }
+        public int TareasCompletadasEstudiante { get; set; }
+
+        public List<Tarea> TareasPersonales { get; set; }
+
     }
 
-    public class ReporteEstudianteProyecto
+    public class RepoteFiltros
     {
-        public string NombreEstudiante { get; set; }
-        public int TareasAsignadas { get; set; }
-        public int TareasCompletadas { get; set; }
-        public decimal PorcentajeCompletado { get; set; }
+
+        public int? ProyectoId { get; set; }
+
+        public int? EsrudianteId { get; set; }
+
+        public int? UsuarioId { get; set; }
+
+        public DateTime? FechaInicio { get; set; }
+
+        public DateTime? FechaFin { get; set; }
 
 
     }
-
-    public class ReportePersonal
-    {
-        public string NombrePersonal { get; set; }
-        public string NombreTarea { get; set; }
-        public string Estado { get; set; }
-        public DateTime? FechaCompletado { get; set; }
-        public DateTime FechaVencimiento { get; set; }
-
-    }
-
 }
-
     
     
