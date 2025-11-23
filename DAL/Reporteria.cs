3aabfd1304d87e7a673719f01f46c67f3c78afb2
Vale aguero <valeryaguero27@gmail.com>
2025-11-23 14:45:43 -0600

    Update Reporteria.cs

--- DAL/Reporteria.cs
+++ DAL/Reporteria.cs
@@ -25,15 +25,20 @@ namespace Gestor_de_Proyectos_Académicos.DAL
 
             using (var connection = new SqlConnection(_connectionString))
             {
-                using (var command = new SqlCommand("SP_ReporteProyectos",connection))
+                using (var command = new SqlCommand("SP_ReporteProyectos", connection))
 
                 {
                     command.CommandType = CommandType.StoredProcedure;
-                    command.Parameters.AddWithValue("@ProyectosId", proyectoId);
 
+                    if (filtros.FechaInicio.HasValue)
+                        command.Parameters.AddWithValue("@FechaInicio", filtros.FechaInicio);
+
+
+                    if (filtros.FechaFin.HasValue)
+                        command.Parameters.AddWithValue("@FechaFin", filtros.FechaFin);
 
                     connection.Open();
-                    using(var reader = command.ExecuteReader())
+                    using (var reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
@@ -41,6 +46,7 @@ namespace Gestor_de_Proyectos_Académicos.DAL
                             {
                                 EstudianteId = reader.GetInt32("EstudianteId"),
                                 NombreEstudiate = reader.GetString("NombreEstudiante"),
+                                TotalTareas = reader.GetInt32("TotalTareas"),
                                 TareasCompletadasEstudiante = reader.GetInt32("TareasCompletadas"),
                                 PorcentajeAvance = reader.GetDecimal("PorcentajeAvancePersonal")
                             });
@@ -53,5 +59,92 @@ namespace Gestor_de_Proyectos_Académicos.DAL
             }
             return reporte;
         }
+
+        //Reporte de estudiantes en proyecto 
+        public List<Reporte> ObtenerReporteEstudiantesProyecto(int proyectoId)
+        {
+
+            var reportes = new List<Reporte>();
+
+            using (var connection = new SqlConnection(_connectionString))
+
+            {
+                using (var command = new SqlCommand("SP_ReporteEstudiantesProyecto", connection))
+
+                {
+
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@ProyectoId", proyectoId);
+
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            reportes.Add(new Reporte
+                            {
+
+                                EstudianteId = reader.GetInt32("EstudianteId"),
+                                NombreEstudiante = reader.GetString("NombreEstudiante"),
+                                TareasCompletadasEstudiante = reader.GetInt32("TareasCompletadas"),
+                                ProyectoId = proyectoId,
+                                NombreProyecto = reader.GetString("NombreProyecto")
+
+                            });
+                        }
+
+                    }
+
+                }
+
+                return reportes;
+            }
+        }
+
+        private List<Reporte> ObtenerReportePersonal(int estudianteId)
+        {
+
+            var reportes = new List<Reporte>();
+
+
+            using (var connection = new SqlConnection(_connectionString))
+
+            {
+
+                using (var command = new SqlCommand("SP_ReportePersonalEstudiante", connection))
+                {
+
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@EstudianteId", estudianteId);
+
+
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
+                    {
+
+                        while (reader.Read())
+                        {
+                            reportes.Add(new Reporte
+                            {
+                                ProyectoId = reader.GetInt32("ProyectoId"),
+                                NombreProyecto = reader.GetString("NombreProyecto"),
+                                TotalTareas = reader.GetInt32("TotalTareasPersonales"),
+                                TareasCompletadasEstudiante = reader.GetInt32("TareasCompletadasPersonales"),
+                                PorcentajeAvance = reader.GetDecimal("PorcentajeAvancePersonal")
+
+                            });
+
+                        }
+
+                    }
+                }
+
+                return reportes;
+            }
+        }
     }
 }
+    
+            
+
+          
