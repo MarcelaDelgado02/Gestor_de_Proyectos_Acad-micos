f32df5e41e16521192c15027b5fe3b1e58c3b41a
Vale aguero <valeryaguero27@gmail.com>
2025-11-25 17:35:43 -0600

    Reporte_Estudiantes
    se crea Reportes/Estudiantes

---
+++ Pages/Reporte_Estudiantes/Reporte.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Gestor_de_Proyectos_Académicos.Pages.Reporte_Estudiantes
+{
+    public class Reporte
+    {
+        internal static object First()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
