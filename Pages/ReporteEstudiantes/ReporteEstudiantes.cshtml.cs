8343355d2ba0058f9e6734dc08e14dde0cb45143
Marcela Abarca Delgado <admarcela5@gmail.com>
2025-12-07 13:45:27 -0600

    feat: crear nuevas vistas y controladores
    se crean las respectivas vistas y controladores para reportes

---
+++ Pages/ReporteEstudiantes/ReporteEstudiantes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gestor_de_Proyectos_Académicos.Pages.ReporteEstudiantes
+{
+    public class ReportePersonalModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Pages/ReporteProyectos/ReporteProyectos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gestor_de_Proyectos_Académicos.Pages.ReporteProyectos
+{
+    public class ReporteProyectosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
