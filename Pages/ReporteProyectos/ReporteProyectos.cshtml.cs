348385a199b25d257254af23d2d47a579b021820
Vale aguero <valeryaguero27@gmail.com>
2025-12-07 14:31:37 -0600

    ReporteProyecto
    se crea la vista y el controlador de: ReporteProyecto

--- Pages/ReporteProyectos/ReporteProyectos.cshtml.cs
+++ Pages/ReporteProyectos/ReporteProyectos.cshtml.cs
@@ -1,12 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Gestor_de_Proyectos_Académicos.BLL;
+using System;
+using System.ComponentModel.Design;
 
 namespace Gestor_de_Proyectos_Académicos.Pages.ReporteProyectos
 {
     public class ReporteProyectosModel : PageModel
     {
-        public void OnGet()
+       private readonly ReporteriaBll bll = new ReporteriaBll();    
+
+        public IActionResult Index(DateTime? fechaInicio, DateTime? fechafin)
         {
+            var filtros = new Reporte
+            {
+                fechaInicio = fechaInicio,
+                fechafin = fechafin
+
+            };
+
+            var lista = _bll.ObtenerReporteProyectos(filtros);
+
+            return ViewTechnology(lista);
         }
+
     }
 }
