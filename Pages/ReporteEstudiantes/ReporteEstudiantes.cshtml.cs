a0d76f6db1e8859dfee6cc28ec79c711a744bfbd
Vale aguero <valeryaguero27@gmail.com>
2025-12-07 14:47:09 -0600

    ReporteEstudiante
    Secrea la vista y el controlador  de: ReporteEstudiante

--- Pages/ReporteEstudiantes/ReporteEstudiantes.cshtml.cs
+++ Pages/ReporteEstudiantes/ReporteEstudiantes.cshtml.cs
@@ -1,12 +1,18 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Gestor_de_Proyectos_Académicos.BLL;
 
 namespace Gestor_de_Proyectos_Académicos.Pages.ReporteEstudiantes
 {
     public class ReportePersonalModel : PageModel
     {
-        public void OnGet()
+        private readonly ReporteriaBLL _bll = new ReporteriaBLL();
+
+        public IActionResult Index(int proyectoId)
         {
+            var lista = _bll.ObtenerReporteEstudiantesProyecto(proyectoId);
+            return View(lista);
+
         }
     }
 }
