96b9dc3a92bcc2390307e214330ad2f0fe187339
Vale aguero <valeryaguero27@gmail.com>
2025-12-07 14:37:23 -0600

    ReportePersonal
    se crea la vista y el controlador de: ReportePersonal

--- Pages/ReportePersonal/ReportePersonal.cshtml.cs
+++ Pages/ReportePersonal/ReportePersonal.cshtml.cs
@@ -1,12 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Gestor_de_Proyectos_Académicos.BLL;
 
 namespace Gestor_de_Proyectos_Académicos.Pages.ReportePersonal
 {
     public class ReportePersonalModel : PageModel
     {
-        public void OnGet()
+        private readonly ReporteriaBLL _bll = new ReporteriaBLL();
+
+        public IActionResult Index(int estudianteId)
         {
+            var resultado = _bll.ObtenerReportePersonal(estudianteId);
+            return View(resultado);
         }
     }
 }
